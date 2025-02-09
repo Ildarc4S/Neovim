ae65e514523d6299b9006d111362116c09ed2ac1
Ildar <102915389+Ildarc4S@users.noreply.github.com>
2025-02-09 10:36:42 +0300

    Delete lua directory

--- lua/init.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { import = "plugins.ui" },
-  { import = "plugins.lsp" },
-  { import = "plugins.dap" },
-  { import = "plugins.git" },
-  { import = "plugins.coding" },
-  { import = "plugins.telescope" },
-  { import = "plugins.terminal" },
-}
